63675783e49be16f9d6bce3f6476924fccd5e3a8
Glenn <edstrom.glenn@gmail.com>
2025-06-25 04:36:26 -0500

    Implemented AutoGrad engine with error propagation. Limited calculations right now; I just need to fill in a few more functions and it'll be done

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Error-Propagation
 Datatype to handle error Propagation using the rules for multiplication, division, addition, subtraction, and exponents. It also shows its work as you go so it's easier to learn and study.
 
+## Motivation
+Original project was for making a error propagation calculator that shows work to do these tedious calculations for me in my engineering classes
+Years later, I've come back to update the project with auto-grad style automatic differentiation to clean up the edge cases (where the simplified multiplicitive, additive, etc. rules fail)
+and to test error propagation on neural networks too. Inspired by Andrej Karpathy's micrograd video because it made me remember this project to redo it properly.
+
 It works just by operator overloading all of the operators in the err class to break it up into smaller parts and then it just calculates it in the regular Python order of operations. It prints every step that it computes inside of each operator overload so that you can visualize what it is doing.
 
 # How to run
@@ -15,15 +20,15 @@ The warnings I added occur when you use a number that isn't an err type like the
 
 # Example:
 
-
-└─$ python3 errorProp.py 
+```py
+└─$ python3 errorProp.py
 Enter a variable; none to continue: r
 r = 34
 r = 34 ± .75
 Enter a variable; none to continue: h
 h = 124
 h = 124 ± 2.5
-Enter a variable; none to continue: 
+Enter a variable; none to continue:
 
 Type your equations below, and ctrl-c to exit
 
@@ -38,3 +43,4 @@ derivative =  2.0 * 34.0 ^( 1.0 ) = 68.0
 sqrt(( 51.0 / 1156.0 )^2 + ( 0.0 / 3.141592653589793 )^2)* 3631.681107549801 = 160.22122533307945
 sqrt(( 160.22122533307945 / 3631.681107549801 )^2 + ( 2.5 / 124.0 )^2)* 450328.4573361753 = 21843.68958900029
 pi*r**2*h = 450328.4573361753 ± 21843.68958900029 ( % 4.8506 )
+```
--- engine.py
+++ engine.py
@@ -17,6 +17,13 @@ The motivation for this autograd update is to:
 # This is because you likely want to know the error of your predicted values rather than of the loss function
 #WARN: If you compute both the loss.backward() to update gradients and y_pred.backward(), make sure to zero_grad before every call to .backward()
 
+
+#NOTE: Use the local errors and gradients to propagate errors through the calculation tree.
+# EX: 
+# x1 and x2 have some error
+# you did y_pred.backward()
+# Use all input variables & uncertainties with their partial derivatives dy_pred/dx1, dy_pred/dx2 to compute
+
 # I removed sig figs because they are not relevant to this update and didn't work before
 
 class err():
@@ -29,10 +36,46 @@ class err():
     * / + - **
     """
 
-    def __init__(self, val, err=0.0):
+    def __init__(self, val, err=0.0, _children=(), _op=''):
         # warn if the input wasn't a string originally
         self.val = float(val)   # number
         self.err = float(err)   # plus/minus error
+        self._op = _op
+        self._children = set(_children)
+        self._backward = lambda: None
+        self._input_nodes = set() # for finding all input nodes for error prop
+        self.grad = 0
+
+    def _topsort(self, children, visited, input_nodes):
+        if self in visited:
+            return
+        visited.add(self)
+        if self._op == '':
+            input_nodes.add(self)
+
+        for child in self._children:
+            child._topsort(children, visited, input_nodes)
+        children.append(self)
+
+    def backward(self):
+        self.grad = 1
+        #top sort the nodes
+        children = []
+        self._topsort(children, set(), self._input_nodes)
+        #call _backward() in order on all nodes
+        for child in children[::-1]:
+            child._backward()
+
+    def zero_grad(self):
+        #zero out all the gradients; lazy method
+        self.grad = 0
+
+        children = []
+        self._topsort(children, set(), self._input_nodes)
+        self._input_nodes = set() # reset input nodes too
+
+        for child in children[::-1]:
+            child.grad = 0
 
     @classmethod
     def _ensure_err(cls, o):
@@ -41,74 +84,141 @@ class err():
             return cls(str(o), 0)
         return o
 
+    def get_err(self):
+        # use _input_nodes to get the error
+        total = 0
+        for node in self._input_nodes:
+            total += (node.grad * node.err)**2
+        total = total**0.5
+        return total
+
     # operation overloading
 
     def __add__(self, o):
         ans = self.val + o.val
+        out = err(ans, _children=(self,o), _op='+')
+        def _backward():
+            self.grad += out.grad
+            o.grad += out.grad
 
-        return err(ans, math.sqrt(self.err**2 + o.err**2))
+        out._backward = _backward
+        return out
     __radd__ = __add__
 
     def __sub__(self, o):
         o = self._ensure_err(o)
-
         ans = self.val - o.val
+        out = err(ans, _children=(self,o), _op='-')
+        def _backward():
+            self.grad += out.grad
+            o.grad += -out.grad
+        out._backward = _backward
 
-        return err(ans, math.sqrt(self.err**2 + o.err**2))
+        return out
 
     def __rsub__(self, o):
         o = self._ensure_err(o)
-
         ans = o.val - self.val
-
-        return err(ans, math.sqrt(self.err**2 + o.err**2))
+        out = err(ans, _children=(self,o), _op='-')
+        def _backward():
+            o.grad += out.grad
+            self.grad += -out.grad
+        out._backward = _backward
+        return out
 
     def __mul__(self, o):
         o = self._ensure_err(o)
-
         ans = self.val * o.val
+        out = err(ans, _children=(self,o), _op='*')
+        def _backward():
+            o.grad += self.val * out.grad
+            self.grad += o.val * out.grad
+        out._backward = _backward
 
-        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
-        return err(ans, error)
+        return out
     __rmul__ = __mul__
 
     def __truediv__(self, o):
         o = self._ensure_err(o)
         ans = self.val / o.val
-        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
-        return err(ans, error)
+        out = err(ans, _children=(self,o), _op='/')
+        # def _backward():
+        #     o.grad += self.val * out.grad
+        #     self.grad += o.val * out.grad
+        # out._backward = _backward
+        return out
 
     def __rtruediv__(self, o):
         o = self._ensure_err(o)
         ans =  o.val / self.val
-        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
-        return err(ans, error)
+        out = err(ans, _children=(self,o), _op='/')
+        # def _backward():
+        #     o.grad += self.val * out.grad
+        #     self.grad += o.val * out.grad
+        # out._backward = _backward
+        return out
 
     # TODO: Redo these with autograd, then exponents won't have to be exact
     def __pow__(self, o):
-        if type(o) != err: #exponents need to be exact
-            o = err(str(o), 0)
-        elif o.err != 0:# x**n; where n needs to have no error
-            warnings.warn("WARNING: your exponent needs to be exact; ignoring error...")
+        o = self._ensure_err(o)
         ans = self.val ** o.val
-        derivative = o.val*self.val**(o.val-1)
-        error = abs(derivative)*self.err
-        return err(ans, error)
+        out = err(ans, _children=(self,o), _op='**')
+        # def _backward():
+        #     o.grad += self.val * out.grad
+        #     self.grad += o.val * out.grad
+        # out._backward = _backward
+        return out
 
     def __rpow__(self, o):
-        if type(o) != err: #exponents need to be exact
-            o = err(str(o), 0)
-        elif self.err != 0:# x**n; where n needs to have no error
-            warnings.warn("WARNING: your exponent needs to be exact; ignoring error...")
-
+        o = self._ensure_err(o)
         ans =  o.val ** self.val
-        derivative = self.val*o.val**(self.val-1)
-        error = abs(derivative)*o.err
-        return err(ans, error)
+        out = err(ans, _children=(self,o), _op='**')
+        # def _backward():
+        #     o.grad += self.val * out.grad
+        #     self.grad += o.val * out.grad
+        # out._backward = _backward
+        return out
     #output formatting
     #def __str__(self):
     #    return str(self.val) + " ± " + str(self.err) + " ( % " + str(round(self.err/self.val*100,4)) + " )"
 
     def __repr__(self):
         return "err(" + str(self.val) + ", err=" + str(self.err) + ")"
+
+
+
+if __name__ == "__main__":
+    y_true=157
+    a = err(5, .02)
+    b = err(10, .02)
+    ab = a*b
+    c = err(3.1415926535)
+    y_pred = ab*c
+
+    L = 1/1*(y_true-y_pred)**2
+
+    # Normal backprop to update weights:
+    # L.zero_grad()
+    # L.backward()
+    # optimizer.step()
+    print(L)
+    #
+    # # Get Uncertainty:
+    # y_pred.zero_grad()
+    # y_pred.backward()
+    y_pred.backward()
+    # y_pred.get_err() # can do this for every variable; This is a function that computes the error assuming the gradient was calculated with y_pred (if you want your prediction's uncertainty)
+    print(a.grad, b.grad, c.grad, ab.grad, y_pred.grad)
+    print(y_pred.get_err())
+
+    # proof with definition of the derivative
+    h = 0.00001
+    print((((5+h)*(10)*(3.1415926535))-(5*10*3.1415926535))/h)
+    print((((5)*(10+h)*(3.1415926535))-(5*10*3.1415926535))/h)
+    print((((5)*(10)*(3.1415926535+h))-(5*10*3.1415926535))/h)
+    print()
+    print("Error of the solution y_pred and the percent error:")
+    print(y_pred.get_err())
+    print(y_pred.get_err()/y_pred*100)
+    
 
